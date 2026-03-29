5178fc8d965393f6916eab2b374e4aac97e58b90
Kshitij Chandankhede <chandankhede.kshitij@gmail.com>
2026-03-30 01:06:54 +0530

    Adding the Save Game File
    this is the USaveGame Child class where we will save all of our Games Saveable Data

---
+++ Source/ModularSaveSystem/Private/SaveGame/SaveGameData.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "SaveGameData.generated.h"
+
+/**
+ * 
+ */
+USTRUCT()
+struct FActorSaveData
+{
+	GENERATED_BODY()
+
+	UPROPERTY()// no need for SaveGame keyword since this struct will be only used as key value for a TMap UObject value which is marked with SaveGame.
+	TMap<FString, FString> SaveData;
+
+	FActorSaveData(){};
+
+	FActorSaveData(const TMap<FString, FString>& InSaveData): SaveData(InSaveData){};
+	
+};
+
+
+UCLASS()
+class USaveGameData : public USaveGame
+{
+	GENERATED_BODY()
+
+public:
+ 	UPROPERTY(SaveGame)
+	TMap<FGuid, FActorSaveData> GameData;
+
+};
